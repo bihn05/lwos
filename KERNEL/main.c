3e6271f28dbc37459a82e93f809008c3b7d6ef78
iRstek05 <42379396+bihn05@users.noreply.github.com>
2025-03-11 16:05:03 +0800

    Add files via upload

--- KERNEL/main.c
+++ KERNEL/main.c
@@ -1,7 +1,11 @@
 #include <lwos.h>
 
-int a = LWOS_MAGIC;
+int magic = LWOS_MAGIC;
+char message[] = "ITS WORKING.";
 
 void kernel_init() {
-;
+	char* video = (char*)0xb8000 + 160;
+	for (int i=0; i<sizeof(message); i++) {
+		video[i*2] = message[i];
+	}
 }
