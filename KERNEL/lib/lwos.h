782f9b478fad410fc53d8e79967d141d4698e428
rist <1578349858@qq.com>
2025-03-16 09:56:01 +0800

    deleted rubbish

--- KERNEL/lib/lwos.h
+++
@@ -1,8 +0,0 @@
-#ifndef LWOS_H
-#define LWOS_H
-
-#define LWOS_MAGIC 0x7777
-
-void kernel_init();
-
-#endif
--- print.h
+++
@@ -1,6 +0,0 @@
-#ifndef __LIB_KERNEL_PRINT_H
-#define __LIB_KERNEL_PRINT_H
-
-extern int add_num(int a, int b);
-
-#endif
