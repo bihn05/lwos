0cf86ceff1e7bbaa14df6c2b4e1e379ff0ad5907
iRstek05 <42379396+bihn05@users.noreply.github.com>
2025-03-16 09:59:09 +0800

    Add files via upload

--- make.cmd
+++ make.cmd
@@ -1,7 +1,6 @@
 echo off
 
 :st
-
 echo ---------------------Clean Stage
 
 del /q master.img
@@ -13,7 +12,13 @@ echo ---------------------Base System
 nasm -f bin MBR.S -o MBR.BIN
 nasm -f bin FAT.S -o FAT.BIN
 nasm -f bin FDT.S -o FDT.BIN
-nasm -f bin LWOS.S -o LWOS.BIN
+cd KERNEL
+nasm -f win32 START.S -o START.o
+gcc -m32 -fno-builtin -nostdinc -fno-pic -fno-pie -nostdlib -fno-stack-protector -I. -c main.c -o main.o
+ld -m i386pe -static  ./START.o ./main.o -o ./kernel.bin -Ttext 0x70000
+cd..
+objcopy -O binary ./KERNEL/kernel.bin system.bin
+nm ./KERNEL/kernel.bin | sort > system.map
 
 set /p cs=Continue or Retry? (c/r)
 if /i "%cs%" == "r" (
@@ -30,7 +35,7 @@ dd bs=512 count=128 seek=1 if=FAT.BIN of=master.img
 dd bs=512 count=128 seek=129 if=FDT.BIN of=master.img
 dd bs=512 count=1 seek=257 if=WHEEL.BI_ of=master.img
 dd bs=512 count=8 seek=258 if=BASFNT.FNT of=master.img
-dd bs=512 count=2 seek=266 if=LWOS.BIN of=master.img
+dd bs=512 count=128 seek=270 if=system.bin of=master.img
 
 echo ---------------------Power On
 
